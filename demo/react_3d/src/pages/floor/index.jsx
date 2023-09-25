334d42bf132d6fd54b4449ded7d2a89498b7ecca
wsj <1207050801@qq.com>
2023-09-25 15:09:12 +0800

    feat:add shadow

--- demo/react_3d/src/pages/floor/index.jsx
+++ demo/react_3d/src/pages/floor/index.jsx
@@ -5,7 +5,7 @@ import floor from '../../assets/lineFloor.png';
 import * as THREE from 'three';
 
 function App() {
-    //创建场景
+    //创建场景loaderCylinderGeometryCylinderGeometry
     const scene = new THREE.Scene();
 
     //创建相机
@@ -18,25 +18,36 @@ function App() {
 
     //创建渲染器
     const renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     //创建立方体
     const geometry = new THREE.PlaneGeometry(5, 5);
+    const pdGeometry = new THREE.CylinderGeometry(1, 1, 2, 8);
+
     const texLoader = new THREE.TextureLoader();
     const picture = texLoader.load(floor);
     picture.wrapS = THREE.RepeatWrapping;
     picture.wrapT = THREE.RepeatWrapping;
     picture.repeat.set(2, 2);
     //材质
-    const material = new THREE.MeshBasicMaterial({ map: picture,side:THREE.DoubleSide });
+    const material = new THREE.MeshLambertMaterial({ map: picture, side: THREE.DoubleSide });
+    const pdMaterial = new THREE.MeshLambertMaterial({ color: 0xffff00 });
     //网格
     const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const pdCube = new THREE.Mesh(pdGeometry, pdMaterial);
+    cube.rotateX(Math.PI / 2);
+    cube.receiveShadow=true;
+    pdCube.position.set(0, 1, 0)
+    pdCube.castShadow = true;
+    scene.add(cube, pdCube);
 
     //平行光源
-    const directionLight = new THREE.DirectionalLight(0xffffff, 2);
+    const directionLight = new THREE.DirectionalLight(0xffffff, 4);
     directionLight.position.set(3, 3, 3)
+    directionLight.castShadow = true;
     scene.add(directionLight);
 
     //相机控件
@@ -44,8 +55,8 @@ function App() {
     // controls.update();
 
     //z轴对着自己 设置视角距离
-    camera.position.z = 10;
-    camera.position.x = 10;
+    camera.position.z = 3;
+    camera.position.x = 3;
     camera.position.y = 5;
 
     //添加世界坐标辅助器
